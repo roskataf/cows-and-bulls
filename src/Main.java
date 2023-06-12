0a563cae576ffc1f468034ce124b4b99765e3de0
roskataf <azwell1@abv.bg>
2023-06-12 22:26:25 +0300

    Created a method for generating random number with 4 digits, every with special digit. Added choice for single- or multiplayer and checking if the input is possible. If not, the console outputs an error.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Random;
+import java.util.Scanner;
 
+public class Main {
+
+    public static int generateNumber() {
+        Random random = new Random();
+
+        int uniqueNumber = random.nextInt(9) + 1;
+
+        for (int i = 0; i < 3; i++) {
+            int digit = random.nextInt(9);
+
+            while (String.valueOf(uniqueNumber).contains(String.valueOf(digit))) {
+                digit = random.nextInt(9);
+            }
+
+            uniqueNumber = uniqueNumber * 10 + digit;
+        }
+
+        return uniqueNumber;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Cows and Bulls!");
+        System.out.print("Do you want to play single-(1) or multiplayer(2): ");
+
+        int choice = scanner.nextInt();
+        if (choice == 1) {
+            singleplayer();
+        } else if(choice == 2) {
+            multiplayer();
+        } else {
+            System.out.println("Not a valid option.");
+        }
     }
 }
