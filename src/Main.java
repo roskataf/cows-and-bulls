00f02f8803c0031504986f3f51b27ab5d6798d5e
roskataf <azwell1@abv.bg>
2023-06-23 18:39:49 +0300

    Changed the names of the variables, added variables that are going to be used in almost every method.

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,29 @@
+import java.sql.SQLOutput;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    String secretNumber;
+    int guesses;
+    boolean isMultiplayer;
 
     public static int generateNumber() {
         Random random = new Random();
 
-        int uniqueNumber = random.nextInt(9) + 1;
+        int secretNumber = random.nextInt(9) + 1;
 
         for (int i = 0; i < 3; i++) {
             int digit = random.nextInt(9);
 
-            while (String.valueOf(uniqueNumber).contains(String.valueOf(digit))) {
+            while (String.valueOf(secretNumber).contains(String.valueOf(digit))) {
                 digit = random.nextInt(9);
             }
 
-            uniqueNumber = uniqueNumber * 10 + digit;
+            secretNumber = secretNumber * 10 + digit;
         }
 
-        return uniqueNumber;
+        return secretNumber;
     }
 
     public static void main(String[] args) {
@@ -27,12 +32,6 @@ public class Main {
         System.out.print("Do you want to play single-(1) or multiplayer(2): ");
 
         int choice = scanner.nextInt();
-        if (choice == 1) {
-            singleplayer();
-        } else if(choice == 2) {
-            multiplayer();
-        } else {
-            System.out.println("Not a valid option.");
-        }
+
     }
 }
