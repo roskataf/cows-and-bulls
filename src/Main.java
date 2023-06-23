df9d84e3b0e228f16e12902dcfb0ce51d9156407
roskataf <azwell1@abv.bg>
2023-06-23 18:52:43 +0300

    Added a method checkGuess(), which checks if the numbers has the digit of the player's guess. At the end it prints out the cows and the bulls.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,56 @@
+import java.sql.DataTruncation;
 import java.sql.SQLOutput;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    String secretNumber;
-    int guesses;
-    boolean isMultiplayer;
+    static String secretNumber = String.valueOf(generateNumber());
+    static int attempts;
+    static boolean isMultiplayer;
 
+    public static void checkGuess(String guess) {
+        int bulls = 0;
+        int cows = 0;
+
+        for (int i = 0; i < guess.length(); i++) {
+            char digit = guess.charAt(i);
+
+            if (digit == secretNumber.charAt(i)) {
+                bulls++;
+            }
+            else if (secretNumber.contains(String.valueOf(digit))) {
+                cows++;
+            }
+        }
+
+        System.out.println("Bulls: " + bulls);
+        System.out.println("Cows: " + cows);
+    }
+    public static void play(int choice, Scanner scanner) {
+        attempts = 0;
+        if (choice == 1) {
+            isMultiplayer = false;
+        } else if(choice == 2) {
+            isMultiplayer = true;
+        } else {
+            System.out.println("Not a valid option.");
+        }
+
+        while (true) {
+            String guess = scanner.nextLine();
+
+            checkGuess();
+
+            if (bulls = 4) {
+                System.out.println("You won the game!");
+                System.out.println("The number was: " + secretNumber);
+                System.out.println("Total attempts: " + attempts);
+                break;
+            }
+            attempts += 1;
+        }
+    }
     public static int generateNumber() {
         Random random = new Random();
 
@@ -32,6 +75,8 @@ public class Main {
         System.out.print("Do you want to play single-(1) or multiplayer(2): ");
 
         int choice = scanner.nextInt();
+
+        play(choice, scanner);
 
     }
 }
